39985912ffa6fdbb9e4a26c29c6e33a92ff727b3
rjgcssw <3071311441@qq.com>
2018-04-17 19:02:35 +0800

    q

--- src/main/java/com/wanghuan/security/WebsecurityConfig.java
+++ src/main/java/com/wanghuan/security/WebsecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +16,7 @@ public class WebsecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private MyUserDetailsService userDetailsService;
+
 
 	@Autowired
 	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
